8d63fd2c6a282eab1b2294a7d0b826c954603000
cagla eksi <caglaeksii@gmail.com>
2025-08-04 14:11:25 +0300

    refactor: Reuse project cards with props for cleaner structure

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+
+const projects = [
+    {
+        id: 1,
+        title: "Movie Search App",
+        description: "Search movies using the OMDB API",
+        link: "https://github.com/ccagla/movie-search-app"
+    },
+    {
+        id: 2,
+        title: "Form Validator",
+        description: "Input Validation with Vanilla JS",
+        link: "https://github.com/ccagla/form-validator-app"
+    },
+    {
+        id: 3,
+        title: "Mini Gallery App",
+        description: "A simple and responsive image gallery with a modal preview feature",
+        link: "https://github.com/ccagla/mini-gallery-app"
+    },
+    {
+        id: 4,
+        title: "Pomodoro Timer",
+        description: "A simple Pomodoro Timer built with HTML, CSS and Javascript",
+        link: "https://github.com/ccagla/pomodoro-timer"
+    },
+
+]
+
+export default projects
