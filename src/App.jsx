9b963973ba160be449e95cfa245701a7ebf9f7fe
cagla eksi <caglaeksii@gmail.com>
2025-08-04 10:51:20 +0300

    feat: Add about section to portfolio with styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
     <>
      
    <Header />
-      
+      <About/>
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+
+import './About.css';
+
+function About() {
+    return (
+    <section className='about'>
+        <h2>About</h2>
+        <p>Hello! I'm Çağla. I'm a developer who constantly improves herself in the front-end world and prioritizes writing clean and understandable code.
+I'm proficient in core technologies like HTML, CSS, and JavaScript. I'm currently learning React, and I'm excited to discover something new in every project.</p>
+
+    </section>
+    )
+}
+
+export default About;
