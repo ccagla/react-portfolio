3666bf91067cc219e7c6784edd860e9820ed6799
cagla eksi <caglaeksii@gmail.com>
2025-08-04 14:31:10 +0300

    feat: add all Github projects using ProjectCard component with props

--- src/data/projects.js
+++ src/data/projects.js
@@ -24,6 +24,54 @@ const projects = [
         description: "A simple Pomodoro Timer built with HTML, CSS and Javascript",
         link: "https://github.com/ccagla/pomodoro-timer"
     },
+    {
+        id: 5,
+        title: "Personal Landing Page",
+        description: "Personal landing page built with HTML and Tailwind CSS",
+        link: "https://github.com/ccagla/personal-landing-page"
+    },
+    {
+        id: 6,
+        title: "Notes App",
+        description: "A simple yet functional Notes App.All notes are stored in the browser’s localStorage",
+        link: "https://github.com/ccagla/notes-app"
+    },
+    {
+        id: 7,
+        title: "Blog Post App",
+        description: "A simple blog post app. Posts are stored in LocalStorage",
+        link: "https://github.com/ccagla/blog-post-app"
+    },
+    {
+        id: 8,
+        title: "Product Listing App",
+        description: "A simple product listing web app that fetches product from an external API and displays them as cards.",
+        link: "https://github.com/ccagla/product-listing-app"
+    },
+    {
+        id: 9,
+        title: "Flexbox Product Card",
+        description: "This is a responsive product card layout built using HTML and CSS Flexbox. ",
+        link: "https://github.com/ccagla/Flexbox-product-card"
+    },
+    {
+        id: 10,
+        title: "Meal Finder App",
+        description: "Search for meals using TheMealDB API",
+        link: "https://github.com/ccagla/meal-finder-app"
+    },
+    {
+        id: 11,
+        title: "Todo App",
+        description: "Add, delete, save, and restore tasks",
+        link: "https://github.com/ccagla/todo-app"
+    },
+    {
+        id: 12,
+        title: "Basic Profile Card",
+        description: "A clean and responsive personal info card built using semantic HTML and modern CSS techniques.",
+        link: "https://github.com/ccagla/basic-profile-card"
+    },
 
 ]
 
